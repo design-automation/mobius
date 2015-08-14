fa279bae592f14fa823806de33bc8f3908f712de
chepilot <liruizenus@gmail.com>
2015-08-14 17:51:43 +0800

    viewport0.1
    viewport0.1

--- vidamo/directive/viewPort.js
+++ vidamo/directive/viewPort.js
@@ -3,67 +3,62 @@
 //
 
 /* todo */
-// 1.2 elem
-// 1.3 raycaster
-// 1.1.1 size
-// 1.1 zoom/pan/spin
-// 1.2 one windows resize
-// 1.3 lighting
-// 1.4 geometry control
-// 1.5 integration with verbs.js
-// 1.6 clean original 3.js files
+// 1.2 geometry control
+// 1.3 integration with verbs.js
 // 2. gird toggle
 // 3. extend view
 // 4. shading mode
 // 5. set view position
+
 
 vidamo.directive('viewport', function() {
     return {
         restrict: 'ACE',
 
         link: function (scope, elem) {
+
+            // retrieve the viewport dom element
+            var container = elem[0];
+            var VIEWPORT_WIDTH = container.offsetWidth;
+            var VIEWPORT_HEIGHT = container.offsetHeight;
+
             var scene,
                 camera,
                 renderer,
-                container,
                 controls;
 
             init();
             animate();
 
-            function init() { // Initialization
 
+            // Initialization
+            function init(){
                 // create scene
                 scene = new THREE.Scene();
 
-                var VIEWPORT_WIDTH = window.innerWidth,
-                    VIEWPORT_HEIGHT = window.innerHeight;
-
                 // prepare camera
-                var VIEW_ANGLE = 45, ASPECT = VIEWPORT_WIDTH / VIEWPORT_HEIGHT, NEAR = 1, FAR = 10000;
+                var VIEW_ANGLE = 45,
+                    ASPECT = VIEWPORT_WIDTH / VIEWPORT_HEIGHT,
+                    NEAR = 1,
+                    FAR = 10000;
+
                 camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-                scene.add(this.camera);
+                scene.add(camera);
                 camera.position.set(-1200, 600, 1600);
                 camera.lookAt(new THREE.Vector3(0,0,0));
 
                 // prepare renderer
                 renderer = new THREE.WebGLRenderer({antialias:true, alpha: false});
                 renderer.setSize(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-                renderer.setClearColor(0xffffff);
+                renderer.setClearColor(0xe0e0e0);
 
                 renderer.shadowMapEnabled = true;
                 renderer.shadowMapSoft = true;
 
-                // prepare container
-                container = elem[0];
-                container.appendChild(renderer.domElement);
-
-                // events
-                THREEx.WindowResize(renderer, camera);
-
                 // prepare controls (OrbitControls)
                 controls = new THREE.OrbitControls(camera, renderer.domElement);
                 controls.target = new THREE.Vector3(0, 0, 0);
+                container.appendChild(renderer.domElement);
 
                 // add point light
                 var pointLight = new THREE.PointLight(0xffff00, 1.0);
@@ -77,7 +72,32 @@ vidamo.directive('viewport', function() {
                 gridHelper.position = new THREE.Vector3(0, 0, 0);
                 gridHelper.rotation = new THREE.Euler(0, 0, 0);
                 scene.add(gridHelper);
+            }
 
+
+            // monitoring viewport size change
+            scope.$watch(
+                function () {
+                    return {
+                        width: elem[0].offsetWidth,
+                        height: elem[0].offsetHeight
+                    }
+                },
+                function () {
+                    VIEWPORT_WIDTH = container.offsetWidth;
+                    VIEWPORT_HEIGHT = container.offsetHeight;
+                    resizeUpdate();
+                    animate();
+                },
+                true
+            );
+
+            // update on resize of viewport
+            function resizeUpdate() {
+                renderer.setSize(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+                container.appendChild(renderer.domElement);
+                camera.aspect = VIEWPORT_WIDTH / VIEWPORT_HEIGHT;
+                camera.updateProjectionMatrix ();
             }
 
             // Animate the scene
