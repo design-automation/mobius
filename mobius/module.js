83f23acba2ff4ef97db1de6afef94b369bd02710
akshatamohanty <akshatamohanty@gmail.com>
2015-11-05 09:13:20 +0800

    rgb function

--- mobius/module.js
+++ mobius/module.js
@@ -30,6 +30,19 @@ var VIDAMO = ( function (mod){
 		}
 	};
 
+	mod.rgbToHex = function(red, green, blue){
+		
+		return '0x'+toHex(red)+toHex(green)+toHex(blue);
+			
+		function toHex(n) {
+			 n = parseInt(n,10);
+			 if (isNaN(n)) return "00";
+			 n = Math.max(0,Math.min(n,255));
+			 return "0123456789ABCDEF".charAt((n-n%16)/16)
+				  + "0123456789ABCDEF".charAt(n%16);
+		}
+	};
+	
 	//
 	//
 	//
