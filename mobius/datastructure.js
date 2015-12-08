7ba1c5e0ea9eb6a92fd41acb470811a47b98eff9
akshatamohanty <akshatamohanty@gmail.com>
2015-12-08 18:40:46 +0800

    frames

--- mobius/datastructure.js
+++ mobius/datastructure.js
@@ -16,37 +16,26 @@ var mObj = function mObj( type ){
 	}
 }
 
-var mObj_frame = function mObj( origin, xaxis, yaxis, zaxis ){
+var mObj_frame = function mObj_frame( origin, xaxis, yaxis, zaxis ){
 
-    //mObj.call(this, 'frame');
+    mObj.call(this, 'frame');
 
-
-    // compute missing axes 
+    // compute missing axes - follow cylic order to maintain consistency
     if( xaxis == undefined )
         xaxis = verb.core.Vec.cross( yaxis, zaxis );
     else if( yaxis == undefined )
-        yaxis = verb.core.Vec.cross( xaxis, zaxis );
+        yaxis = verb.core.Vec.cross( zaxis, xaxis );
     else if( zaxis == undefined )
-        zaxis = verb.core.Vec.cross( yaxis, xaxis );
+        zaxis = verb.core.Vec.cross( xaxis, yaxis );
 
-    this.origin = origin;
-    _xaxis = verb.core.Vec.normalized( xaxis ); this.xaxis = _xaxis; 
-    _yaxis = verb.core.Vec.normalized( yaxis ); this.yaxis = _yaxis;
-    _zaxis = verb.core.Vec.normalized( zaxis ); this.zaxis = _zaxis;
+    // creating unit vectors
+    var _xaxis = verb.core.Vec.normalized( xaxis ); 
+    var _yaxis = verb.core.Vec.normalized( yaxis ); 
+    var _zaxis = verb.core.Vec.normalized( zaxis ); 
 
-
-    var a = -Math.atan( _xaxis[2] / _xaxis[0] ); 
-    var b = -Math.asin( _xaxis[1] ); //Math.tanh( )
-    var c = Math.atan( zaxis[1] / Math.sqrt( zaxis[0]*zaxis[0] + zaxis[2]*zaxis[2] ) )
-
-    // compute translation
-    var mat_trans = [ [ 1, 0, 0, origin[0] ],
-                        [ 0, 1, 0, origin[1] ],
-                            [ 0, 0, 1, origin[2] ],
-                                [ 0, 0, 0, 1 ]
-      
-                        ]; 
-
+    /* If the 3D space is right-handed, this rotation will be counterclockwise when u points towards the observer (Right-hand rule). 
+     * Rotations in the counterclockwise (anticlockwise) direction are considered positive rotations. 
+     */
     function getRotationMatrix( axis, angle){
         var cost = Math.cos(angle);
         var sint = Math.sin(angle);
@@ -59,30 +48,93 @@ var mObj_frame = function mObj( origin, xaxis, yaxis, zaxis ){
                         [ ux*uz*(1-cost) - uy*sint, uy*uz*(1-cost) + ux*sint, cost + uz*uz*(1-cost), 0 ],
                             [ 0, 0, 0, 1 ]
                 ];
-
     }
 
-    // yaxis [0,1,0]
+
+    // computing the angles needed to get the object into position - HINT: align with one axis and deal with one vector at a time
+    //var a = Math.atan2( _xaxis[2] _xaxis[0] ); //negative sign because rotation is acw
+    var a = Math.atan2(xaxis[2], xaxis[1]);
+
+    var b = -Math.asin( _xaxis[1] );
+    var c = -Math.atan( zaxis[1] / Math.sqrt( zaxis[0]*zaxis[0] + zaxis[2]*zaxis[2] ) ); 
+    // going from x-axis to the next vector - follow the order of arguments
+/*    var a = verb.core.Vec.angleBetweenNormalized2d( [_xaxis[0], _xaxis[2]], [1,0] ); // calculating angle between xdirection & projection of xaxis on xz plane
+    var b = (_xaxis[1]?_xaxis[1]<0?-1:1:0)*verb.core.Vec.angleBetween( _xaxis, [_xaxis[0], 0, _xaxis[2]] );
+    var c = verb.core.Vec.angleBetweenNormalized2d( [0,1], [_xaxis[0], _xaxis[2]] );*/
+
+    // yaxis [0,1,0] - Rotate about world up-axis to align with the xaxis
     var mat_rot_a = getRotationMatrix( [0,1,0], a); 
 
-    // zaxis [0,0,1]
+    // zaxis [0,0,1] - Rotate about the local coming out axis to align with it
     var mat_rot_b = getRotationMatrix( _zaxis, b);
 
-    // xaxis [1,0,0]
+    // xaxis [1,0,0] - Rotate about the local x axis to obtain final position
     var mat_rot_c = getRotationMatrix( _xaxis, c);
 
+    // compute translation
+    var mat_trans = [ [ 1, 0, 0, origin[0] ],
+                        [ 0, 1, 0, origin[1] ],
+                            [ 0, 0, 1, origin[2] ],
+                                [ 0, 0, 0, 1 ]
+      
+                        ]; 
                         
 
     this.applyMatrix = function( geom ){ 
-       geom = geom.transform(mat_rot_a); 
-       geom = geom.transform(mat_rot_b); 
-       geom = geom.transform(mat_rot_c);
-       geom = geom.transform( mat_trans );
+       geom = geom.transform(mat_rot_a); console.log(a);
+       //geom = geom.transform(mat_rot_b); 
+       //geom = geom.transform(mat_rot_c);
+       geom = geom.transform( mat_trans ); //translation will be in the end. REFER: Conjugation
        return geom;
     }
 
 
     this.extractThreeGeometry = function(){
+
+        function buildAxis( src, dst, colorHex, dashed ) {
+            var geom = new THREE.Geometry(),
+                mat; 
+
+            if(dashed) {
+                    mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+            } else {
+                    mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+            }
+
+            geom.vertices.push( src.clone() );
+            geom.vertices.push( dst.clone() ); 
+            geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+
+            var axis = new THREE.Line( geom, mat, THREE.LineSegments );
+
+            return axis;
+
+        }
+
+        function buildAxes( length ) {
+            var axes = new THREE.Object3D();
+            
+            axes.is_mObj = true;
+
+            var or = new THREE.Vector3( origin[0], origin[1], origin[2] )
+            var pos_x = new THREE.Vector3( origin[0] + length*_xaxis[0],  origin[1]+length*_xaxis[1],  origin[2]+length*_xaxis[2] );
+            var neg_x = new THREE.Vector3( origin[0] - length*_xaxis[0],  origin[1]-length*_xaxis[1],  origin[2]-length*_xaxis[2] );
+            var pos_y = new THREE.Vector3( origin[0] + length*_yaxis[0],  origin[1]+length*_yaxis[1],  origin[2]+length*_yaxis[2] );
+            var neg_y = new THREE.Vector3( origin[0] - length*_yaxis[0],  origin[1]-length*_yaxis[1],  origin[2]-length*_yaxis[2] );
+            var pos_z = new THREE.Vector3( origin[0] + length*_zaxis[0],  origin[1]+length*_zaxis[1],  origin[2]+length*_zaxis[2] );
+            var neg_z = new THREE.Vector3( origin[0] - length*_zaxis[0],  origin[1]-length*_zaxis[1],  origin[2]-length*_zaxis[2] );
+
+            axes.add( buildAxis( or, pos_x, 0x990000, false ) ); // +X
+            axes.add( buildAxis( or, neg_x, 0x990000, true ) ); // -X
+            axes.add( buildAxis( or, pos_y, 0x009900, false ) ); // +Y
+            axes.add( buildAxis( or, neg_y, 0x009900, true ) ); // -Y
+            axes.add( buildAxis( or, pos_z, 0x0000CC, false ) ); // +Z
+            axes.add( buildAxis( or, neg_z, 0x0000CC, true ) ); // -Z
+
+            return axes;
+
+        }
+
         return buildAxes( 20 );
     }
 
@@ -90,50 +142,7 @@ var mObj_frame = function mObj( origin, xaxis, yaxis, zaxis ){
         return 'frame';
     }
 
-    function buildAxis( src, dst, colorHex, dashed ) {
-        var geom = new THREE.Geometry(),
-            mat; 
-
-        if(dashed) {
-                mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
-        } else {
-                mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
-        }
-
-        geom.vertices.push( src.clone() );
-        geom.vertices.push( dst.clone() ); 
-        geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
-
-        var axis = new THREE.Line( geom, mat, THREE.LineSegments );
-
-        return axis;
-
-    }
-
-    function buildAxes( length ) {
-        var axes = new THREE.Object3D();
-        axes.is_mObj = true;
-
-        var or = new THREE.Vector3( origin[0], origin[1], origin[2] )
-        var pos_x = new THREE.Vector3( origin[0] + length*xaxis[0],  origin[1]+length*xaxis[1],  origin[2]+length*xaxis[2] );
-        var neg_x = new THREE.Vector3( origin[0] - length*xaxis[0],  origin[1]-length*xaxis[1],  origin[2]-length*xaxis[2] );
-        var pos_y = new THREE.Vector3( origin[0] + length*yaxis[0],  origin[1]+length*yaxis[1],  origin[2]+length*yaxis[2] );
-        var neg_y = new THREE.Vector3( origin[0] - length*yaxis[0],  origin[1]-length*yaxis[1],  origin[2]-length*yaxis[2] );
-        var pos_z = new THREE.Vector3( origin[0] + length*zaxis[0],  origin[1]+length*zaxis[1],  origin[2]+length*zaxis[2] );
-        var neg_z = new THREE.Vector3( origin[0] - length*zaxis[0],  origin[1]-length*zaxis[1],  origin[2]-length*zaxis[2] );
-
-        axes.add( buildAxis( or, pos_x, 0x990000, false ) ); // +X
-        axes.add( buildAxis( or, neg_x, 0x990000, true ) ); // -X
-        axes.add( buildAxis( or, pos_y, 0x009900, false ) ); // +Y
-        axes.add( buildAxis( or, neg_y, 0x009900, true ) ); // -Y
-        axes.add( buildAxis( or, pos_z, 0x0000CC, false ) ); // +Z
-        axes.add( buildAxis( or, neg_z, 0x0000CC, true ) ); // -Z
-
-        return axes;
-
-    }
-
-}
+a}
 
 // mObj Geometry Class
 // geometry is stored in geometry format native to module
